3c745d70be4df3e3926800e030bbbb1ca900b0d0
fashemma007 <fasogbaemmanuel@gmail.com>
2022-06-17 18:46:07 +0100

    Project UdaPeople

--- roles/setup/files/index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end("<h1>Hello from Emmanuel@Ansible</h1>");
-});
-server.listen(3000);
