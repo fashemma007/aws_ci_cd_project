1fb109feaa63f18eed29afe20a43f2a44290ff74
fashemma007 <fasogbaemmanuel@gmail.com>
2022-06-24 02:19:09 +0100

    new leaf

--- .circleci/ansible/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-IFS="-" read -r UDAPEOPLE SIDE MID <<<$(echo $(aws cloudformation list-stacks --query "StackSummaries[*].StackName" --stack-status-filter CREATE_COMPLETE --no-paginate --output text) | awk '{print $NF}')
-echo ${MID}
